8cfa5f6dab008d152b9f4760459932dc2bacb854
dghodse <ghodsednyaneshwar@gmail.com>
2021-07-13 19:01:27 +0530

    side nav

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
 const hamburger = document.getElementById('hamburger');
 const nav = document.getElementById('nav-ul');
 
+const enquiry = document.getElementById('enquiry');
+const form = document.getElementById('side_form');
 
 hamburger.addEventListener('click', () => {
     nav.classList.toggle('show');
-    console.log("sdjkgjsd");
+})
+
+
+enquiry.addEventListener('click',()=> {
+    form.classList.toggle('show_form');
 })
